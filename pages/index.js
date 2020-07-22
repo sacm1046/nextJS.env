62d4fb01b6183a25b3c5df52a49a074b99d94b8d
Sebastian Contreras <sacm1046@gmail.com>
2020-07-22 17:51:27 -0400

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 
-const Home = () => {
+const Home = ({data}) => {
   return (
-    <div>
-      hello
-    </div>
+    <pre>
+      {JSON.stringify(data, null, 4)}
+    </pre>
   )
 }
 export default Home
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://127.0.0.1:5000/vehicles')
+  const data= await res.json()
+  return { data: data }
+}
