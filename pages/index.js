48862ade5d72837a388b4b96c3c53c52904fc609
Sebastian Contreras <sacm1046@gmail.com>
2020-07-22 18:15:12 -0400

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-const Home = () => {
+import { useState, useEffect } from 'react'
+
+const Home = ({ data }) => {
+
+  const [dat, setDat] = useState(data)
+
+  useEffect(() => {
+      const loadData = async () => {
+        const res = await fetch('http://127.0.0.1:5000/vehicles')
+        const json = await res.json()
+        setDat(json)
+      }
+      if(data.length == 0) {
+          loadData()
+      }
+      if (!data) {
+          <div>Loading...</div>
+      }
+  }, [])
+
   return (
     <pre>
-      aa
+      {JSON.stringify(dat, null, 4)}
     </pre>
   )
 }
+export async function getServerSideProps(ctx) {
 
+  !ctx.req && { ownerList: [] }
+  const res = await fetch('http://127.0.0.1:5000/vehicles')
+  const data = await res.json()
+  return { props: { data } }
+}
 export default Home
