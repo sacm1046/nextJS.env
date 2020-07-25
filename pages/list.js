2c74aa974d1b7afe32a6e30c17c7cb9364b24165
Sebastian Contreras <sacm1046@gmail.com>
2020-07-25 00:46:43 -0400

    deploy

--- pages/list.js
+++ pages/list.js
@@ -1,13 +1,30 @@
 import { useState, useEffect } from 'react'
 
-const List = ({products}) => {
-  console.log(products)
-  return <pre>{JSON.stringify(products,null,3)}</pre>
+const List = ({ products }) => {
+
+  const [prod, setProd] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await fetch('https://aact.pythonanywhere.com/products')
+      const data = await res.json()
+      setProd(data)
+    }
+    if (products?.length == 0) {
+      loadData()
+    }
+    //if the owners[0] is undefined show the div with "loading..." content
+    if (!prod?.[0]) {
+      <div>Loading...</div>
+    }
+  }, [])
+
+  return <pre>{JSON.stringify(products, null, 3)}</pre>
 }
 
 List.getInitialProps = async (ctx) => {
   const res = await fetch('https://aact.pythonanywhere.com/products')
   const data = await res.json()
-  return { products: data}
+  return { products: data }
 }
 export default List
